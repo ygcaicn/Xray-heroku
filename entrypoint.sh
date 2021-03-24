079fd7f51de2ba8ff02503b47939c6eb80c23dcd
jachin <ygcaicn@gmail.com>
2021-03-24 13:39:50 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,7 +72,7 @@ if [ "$AppName" = "no" ]; then
   echo "不生成分享链接"
 else
   [ ! -d /wwwroot/${Share_Path} ] && mkdir /wwwroot/${Share_Path}
-  path=$(echo -n "${Xray_Path}？ed=2048" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g' -e 's/?/%3F/g')
+  path=$(echo -n "${Xray_Path}?ed=2048" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g' -e 's/\?/%3F/g')
   link="vless://${UUID}@${AppName}.herokuapp.com:443?path=${path}&security=tls&encryption=none&type=ws#${AppName}-herokuapp" 
   echo -n "${link}" | tr -d '\n' > /wwwroot/${Share_Path}/index.html
   cat /wwwroot/${Share_Path}/index.html
